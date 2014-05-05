56e18db6af86749e08ae4750dff69fc9de251ad1
JohnsonLu <johnson4932@gmail.com>
2014-05-05 23:36:11 +0800

    scrollTop and scrollBottom

--- fallzu.jquery.scrollwhere.js
+++ fallzu.jquery.scrollwhere.js
@@ -1,5 +1,28 @@
 (function($) {
     $.fn.scrollWhere = function() {
+        $('.fallzu-scroll-return').hide();
+        $(window).scroll(function() {
+            var returnTop = $('.fallzu-scroll-return-top').stop(true, true);
+            var returnBottom = $('.fallzu-scroll-return-bottom').stop(true, true);
+            var windowHeight = $(this).height();
+            var documentHeight = $(document).height();
+            var scrollHeight = $(this).scrollTop();
+
+            //Top
+            if (scrollHeight) {
+                returnTop.fadeIn();
+            } else {
+                returnTop.fadeOut();
+            }
+
+            //Bottom
+            if (windowHeight + scrollHeight < documentHeight) {
+                returnBottom.fadeIn();
+            } else {
+                returnBottom.fadeOut();
+            }
+        });
+
         return $(this);
     };
 })(jQuery);
